8b6dfff9799e1992788017e98924427c308ff05d
Benjamin Budet <benjamin.budet@epita.fr>
2018-11-03 14:18:25 +0100

    Add type label and detail for pkm

--- Pokédex/ViewControllers/PokedexViewController.swift
+++ Pokédex/ViewControllers/PokedexViewController.swift
@@ -58,8 +58,22 @@ class PokedexViewController: UIViewController, UITableViewDelegate, UITableViewD
         let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath) as! PokemonTableViewCell
         cell.pokemonName.text = pokemons[indexPath.row].name
         cell.pokemonImage.sd_setImage(with: URL(string: "http://pokedex-mti.twitchytv.live/images/\(indexPath.row + 1).png"), placeholderImage: UIImage(named: "pokeball"))
+        cell.pokemonType1.backgroundColor = getColorFromType(type: pokemons[indexPath.row].type1.id)
         cell.pokemonType1.text = pokemons[indexPath.row].type1.name
-        cell.pokemonType2.text = pokemons[indexPath.row].type2?.name ?? "No type2"
+        cell.pokemonType1.layer.cornerRadius = cell.pokemonType1.frame.size.height / 2
+        cell.pokemonType1.clipsToBounds = true
+        cell.pokemonType1.textColor = UIColor.white
+        
+        if let type2 = pokemons[indexPath.row].type2 {
+            cell.pokemonType2.backgroundColor = getColorFromType(type: type2.id)
+            cell.pokemonType2.text = type2.name
+            cell.pokemonType2.layer.cornerRadius = cell.pokemonType2.frame.size.height / 2
+            cell.pokemonType2.clipsToBounds = true
+            cell.pokemonType2.textColor = UIColor.white
+        } else {
+            cell.pokemonType2.text = ""
+        }
+        
         return cell
     }
     
